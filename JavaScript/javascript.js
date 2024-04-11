35d622c03585f7f45a31acad95301afa6d1d1d78
Alexander <143174402+AMehtola@users.noreply.github.com>
2024-04-11 13:06:28 +0300

    k

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -1,26 +1,20 @@
 //explanations
-var favcolor = 'blue';
-var myFavColors = ['blue', 'red', 'green'];
-var numbOfFavColors = '3';
-var hasGotFavColors = true;
-var richObject = {
-    firstname: 'Rich',
-    lastname: 'Armstrong',
-    FavColors: ['blue', 'red', 'green'];
-    ,
-    yearsAlive: 103,
-    isMale: true
-};
-
-var WhatIsMyFavColor = function () {
-    
-    return false;
-
-    return true;
-
-    return Blue;
+var number1 = 10;
+var number2 = 100;
+if (number1 > number2) {
+    alert('true!!!');
+} else {
+    alert('false!!!');
 }
 
-var doSomething = function () {
-    console.log("do something!");
+var name1 = 'Teuvo';
+var name2 = 'Tomi';
+
+if (name1 == 'Teuvo' && name2 == 'Tomi') {
+    alert('true 1 !!!') 
+} else if (name2 == 'Teuvo') {
+    alert('true 2 !!!');
+} else {
+    alert('false!!!');
 }
+
